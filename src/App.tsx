8b644c4bc228e06e478dee88fa71764ebac6ce34
Gordus Alexander <aldrmiller@gmail.com>
2024-05-17 11:52:28 +0600

    реализовал по клику на кнопку смену чисел, а также добавил сортировку от меньшего к большему

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,24 @@ import Ball from './components/Ball/Ball';
 import {useState} from 'react';
 
 const App = () => {
-    const [numbers, setNumbers] = useState([5, 11, 16, 23, 32]);
+    const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
+
+    const generateRandomNumbers = (): void => {
+        const arrayNumbers:number[] = [];
+        for (let i = 0; i < numbers.length; i++) {
+            const number = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+            arrayNumbers.push(number);
+        }
+        arrayNumbers.sort((a, b) => a - b);
+        setNumbers(arrayNumbers);
+    };
 
     return (
         <div className="lotteries-container">
-            <button className="lotteries-btn" type="button">New numbers</button>
+            <button onClick={generateRandomNumbers} className="lotteries-btn" type="button">New numbers</button>
             <div className="lotteries-balls-list">
-                {numbers.map((number) => (
-                    <Ball number={number}/>
+                {numbers.map((number, index) => (
+                    <Ball key={index} number={number}/>
                 ))}
             </div>
         </div>
